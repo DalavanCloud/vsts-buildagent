d40437dfe1977a8bb5785959cdc76182243222aa
marcvaneijk@msn.com <marcvaneijk@msn.com>
2018-08-18 15:01:47 +0200

    link, args and formatting

--- ubuntu1604lts/scripts/setupagent.sh
+++ ubuntu1604lts/scripts/setupagent.sh
@@ -1,35 +1,40 @@
-# Variables
-VSTSURL="https://domain.visualstudio.com"
-PAT=""
-AGENTPOOL=""
+#!/bin/bash
 
-# Install JQ
+# Parameters : Validate that all arguments are supplied
+if [ $# -lt 3 ]; then unsuccessful_exit "Insufficient parameters supplied." 1; fi
+
+# Variables
+VSTSURL=$1
+PAT=$2
+AGENTPOOL=$3
+
+echo "Install JQ"
 sudo apt-get -y install jq
 
-### Azure CLI 2.0 ###
+echo "### Azure CLI 2.0 ###"
 
-# Modify your sources list
+echo "Modify your sources list"
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-# Get the Microsoft signing key
+echo "Get the Microsoft signing key"
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-# Install Azure CLI 2.0 with apt
+echo "Install Azure CLI 2.0 with apt"
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
 
-### VSTS Agent ###
+echo "### VSTS Agent ###"
 
-# Download and extract the agent
+echo "Download and extract the VSTS agent"
 mkdir agent && cd agent
 curl -L https://vstsagentpackage.azureedge.net/agent/2.136.1/vsts-agent-linux-x64-2.136.1.tar.gz | tar zx
 
-# Install .Net core 2.0 dependencies from agent folder
+echo "Install .Net core 2.0 dependencies from agent folder"
 sudo ./bin/installdependencies.sh
 
-# Configure the VSTS agent
+echo "Configure the VSTS agent"
 ./config.sh \
     --unattended \
     --url $VSTSURL \
@@ -40,6 +45,9 @@ sudo ./bin/installdependencies.sh
     --replace \
     --acceptTeeEula
 
-# Configure the agent to start as a service ank start after reboot
+echo "Configure the agent to start as a service ank start after reboot"
 sudo ./svc.sh install
 sudo ./svc.sh start
+
+echo "Commands succeeded. Exiting";
+exit 0;
