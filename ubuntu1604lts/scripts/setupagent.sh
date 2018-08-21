f2f3e22c624dbb5c8c1473c5d289c3db394c706e
marcvaneijk@msn.com <marcvaneijk@msn.com>
2018-08-21 10:38:30 +0200

    remove sudo

--- ubuntu1604lts/scripts/setupagent.sh
+++ ubuntu1604lts/scripts/setupagent.sh
@@ -8,33 +8,38 @@ VSTSURL=$1
 PAT=$2
 AGENTPOOL=$3
 
-echo "Install JQ"
-sudo apt-get -y install jq
+echo "--- Install JQ ---"
+#sudo 
+apt-get -y install jq
 
 echo "### Azure CLI 2.0 ###"
 
-echo "Modify your sources list"
+echo "--- Modify your sources list ---"
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+    tee /etc/apt/sources.list.d/azure-cli.list #sudo 
 
-echo "Get the Microsoft signing key"
+echo "--- Get the Microsoft signing key ---"
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-echo "Install Azure CLI 2.0 with apt"
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+echo "--- Install Azure CLI 2.0 with apt ---"
+# sudo 
+apt-get install apt-transport-https
+# sudo 
+apt-get update && sudo apt-get install azure-cli
 
 echo "### VSTS Agent ###"
 
-echo "Download and extract the VSTS agent"
-mkdir agent && cd agent
-curl -L https://vstsagentpackage.azureedge.net/agent/2.136.1/vsts-agent-linux-x64-2.136.1.tar.gz | tar zx
+echo "--- Download and extract the VSTS agent --"
+#sudo 
+mkdir /agent && cd /agent
+curl -L https://vstsagentpackage.azureedge.net/agent/2.136.1/vsts-agent-linux-x64-2.136.1.tar.gz | sudo tar zx
 
-echo "Install .Net core 2.0 dependencies from agent folder"
-sudo ./bin/installdependencies.sh
+echo "--- Install .Net core 2.0 dependencies from agent folder ---"
+#sudo 
+./bin/installdependencies.sh
 
-echo "Configure the VSTS agent"
+echo "--- Configure the VSTS agent ---"
 ./config.sh \
     --unattended \
     --url $VSTSURL \
@@ -45,9 +50,11 @@ echo "Configure the VSTS agent"
     --replace \
     --acceptTeeEula
 
-echo "Configure the agent to start as a service ank start after reboot"
-sudo ./svc.sh install
-sudo ./svc.sh start
+echo "--- Configure the agent to start as a service ank start after reboot ---"
+#sudo 
+./svc.sh install
+#sudo 
+./svc.sh start
 
-echo "Commands succeeded. Exiting";
-exit 0;
+# echo "Commands succeeded. Exiting";
+# exit 0;
